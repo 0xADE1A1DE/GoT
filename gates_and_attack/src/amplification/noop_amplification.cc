31dd8cd5e03d459810bb9ef73f26058176ad3de8
user <user@example.com>
2023-03-08 09:39:10 +0200

    Remove older code from repo.

--- gates_and_attack/src/amplification/noop_amplification.cc
+++
@@ -1,8 +0,0 @@
-#include "amplification/noop_amplification.h"
-
-namespace amplification {
-
-auto _noop_amplification = NoopAmplification();
-Amplification &noop_amplification = _noop_amplification;
-
-}
